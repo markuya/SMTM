5761777f1357c4e700d241115c1815fbb28ac2ce
赵睿 <zhaorui826@qq.com>
2022-10-27 17:15:13 +0800

    添加 写入功能

--- go-code/lib/primCsv/primCsv.go
+++ go-code/lib/primCsv/primCsv.go
@@ -4,6 +4,8 @@ import (
 	"encoding/csv"
 	"errors"
 	"os"
+
+	"strconv"
 )
 
 // Options is a configuration that is used to create a new CsvReader.
@@ -62,15 +64,15 @@ func (tReader *CsvReader) ReadArr() ([]string, error) {
 		return tStrList, nil
 	}
 
-	return nil, errors.New("handle is nil!")
+	return nil, errors.New("handle is nil! ")
 }
 
 // 逐行读取文件,并以字典表格式返回数据
 func (tReader *CsvReader) ReadDict() (map[string]string, error) {
 	if (nil != tReader.mReader) && (nil != tReader.mFileHandle) {
 		// 未加载或设置标题信息，需加载标题
-		if false == tReader.mHaveHeader {
-			return nil, errors.New("the csv file have no header!")
+		if !tReader.mHaveHeader {
+			return nil, errors.New("the csv file have no header! ")
 		}
 
 		// 读取一行数据
@@ -80,8 +82,8 @@ func (tReader *CsvReader) ReadDict() (map[string]string, error) {
 		}
 
 		// 将行数据转化为字典表
-		if nil == tReader.mHeaderIndex {
-			return nil, errors.New("Header indexs dict is nil!")
+		if tReader.mHeaderIndex == nil {
+			return nil, errors.New(" Header indexs dict is nil! ")
 		}
 
 		//
@@ -91,7 +93,7 @@ func (tReader *CsvReader) ReadDict() (map[string]string, error) {
 		//
 		for tTitle, tIndex := range tReader.mHeaderIndex {
 			if tIndex >= tStrLen {
-				return nil, errors.New("this data num is less then the title!")
+				return nil, errors.New("this data num is less then the title! ")
 			} else {
 				tStrMap[tTitle] = tStrList[tIndex]
 			}
@@ -100,7 +102,7 @@ func (tReader *CsvReader) ReadDict() (map[string]string, error) {
 		return tStrMap, nil
 	}
 
-	return nil, errors.New("handle is nil!")
+	return nil, errors.New("handle is nil! ")
 }
 
 // 读取文件标题
@@ -111,10 +113,10 @@ func (tReader *CsvReader) GetHeader() ([]string, error) {
 			return tReader.mHeader, nil
 		}
 
-		return nil, errors.New("no is header!")
+		return nil, errors.New("no is header! ")
 	}
 
-	return nil, errors.New("handle is nil!")
+	return nil, errors.New("handle is nil! ")
 }
 
 // 读取文件标题
@@ -146,7 +148,7 @@ func (tReader *CsvReader) loadHeader() error {
 		return nil
 	}
 
-	return errors.New("handle is nil!")
+	return errors.New("handle is nil! ")
 }
 
 // 创建新句柄
@@ -179,7 +181,7 @@ func Open(tFileName string, opts ...Options) (*CsvReader, error) {
 	}
 
 	// 判断文件是否有标题
-	if true == tOptions.HaveHeader {
+	if tOptions.HaveHeader {
 		tReader.loadHeader()
 	}
 
@@ -190,4 +192,175 @@ func Open(tFileName string, opts ...Options) (*CsvReader, error) {
 	tReader.mReader.TrimLeadingSpace = tOptions.TrimLeadingSpace
 
 	return tReader, nil
+}
+
+// csv文件写入器
+type CsvWriter struct {
+	mFile       string      // 文件名
+	mWriter     *csv.Writer // 数据写入句柄
+	mFileHandle *os.File    // 当前文件句柄
+	mHeader     []string    // 标题列表
+	mMaxLines   int         // 单文件最大行
+	mNowLines   int         // 当前文件行数
+	mFilesNum   int         // 已写入文件数量
+	mFileList   []string    // 文件列表
+}
+
+// Options is a configuration that is used to create a new CsvWriter.
+type WriterOptions struct {
+	// CSV文件标题
+	Header []string
+
+	// 单文件最大行.
+	MaxLines int
+
+	// Field delimiter (set to ',' by NewWriter)
+	Comma rune
+
+	// True to use \r\n as the line terminator
+	UseCRLF bool
+}
+
+// 打开新的写入器
+func NewWriter(tFileName string, opts ...WriterOptions) (*CsvWriter, error) {
+	// 打开文件
+	tFileHandle, err := os.OpenFile(tFileName, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	tWriter := &CsvWriter{
+		mFile:       tFileName,
+		mFileHandle: tFileHandle,
+		mNowLines:   0,
+		mFilesNum:   1,
+		mWriter:     csv.NewWriter(tFileHandle),
+	}
+	var tOptions *WriterOptions
+	// 未传入配置信息
+	if len(opts) == 0 {
+		tOptions = &WriterOptions{
+			Comma:   ',',
+			UseCRLF: true,
+		}
+	} else {
+		tOptions = &opts[0]
+		if tOptions.Comma == 0 {
+			tOptions.Comma = ','
+			tOptions.UseCRLF = true
+		}
+	}
+
+	tWriter.mMaxLines = tOptions.MaxLines
+	tWriter.mHeader = tOptions.Header
+	//tWriter.mWriter.Comma = tOptions.Comma
+	//tWriter.mWriter.UseCRLF = tOptions.UseCRLF
+
+	// 存储文件添加到文件列表
+	tWriter.mFileList = append(tWriter.mFileList, tFileName)
+
+	return tWriter, nil
+}
+
+// 写入记录
+func (tWriter *CsvWriter) Write(tRecords []string) error {
+	// 句柄检测
+	if (tWriter.mFileHandle == nil) && (tWriter.mWriter == nil) {
+		return errors.New("CsvWriter write failed! the file/writer handle is nil")
+	}
+
+	// 判断是否需要创建新文件
+	if (tWriter.mMaxLines > 0) && (tWriter.mNowLines >= tWriter.mMaxLines) {
+		// 文件写入缓存推送
+		tWriter.mWriter.Flush()
+		// 文件数+1
+		tWriter.mFilesNum++
+		// 当前写入行数重置
+		tWriter.mNowLines = 0
+		// 新打开文件
+		tFileName := tWriter.mFile[:len(tWriter.mFile)-4] + "-" + strconv.Itoa(tWriter.mFilesNum) + tWriter.mFile[len(tWriter.mFile)-4:]
+		tFileHandle, err := os.OpenFile(tFileName, os.O_CREATE|os.O_RDWR, 0666)
+		if err != nil {
+			return err
+		}
+		// 关闭原文件
+		tWriter.mFileHandle.Close()
+		// 新句柄切换
+		tWriter.mFileHandle = tFileHandle
+		tWriter.mWriter = csv.NewWriter(tFileHandle)
+
+		// 如果有文件头
+		if tWriter.mHeader != nil {
+			err := tWriter.mWriter.Write(tWriter.mHeader)
+			if err != nil {
+				tWriter.mFileHandle.Close()
+				return err
+			}
+		}
+
+		// 存储文件添加到文件列表
+		tWriter.mFileList = append(tWriter.mFileList, tFileName)
+	}
+
+	err := tWriter.mWriter.Write(tRecords)
+	if err != nil {
+		return err
+	}
+
+	tWriter.mNowLines++
+
+	return nil
+}
+
+// 多行写入
+func (tWriter *CsvWriter) WriteAll(tRecords [][]string) error {
+	// 逐行写入
+	for _, record := range tRecords {
+		err := tWriter.Write(record)
+		if err != nil {
+			return err
+		}
+	}
+	// 文件写入缓存推送
+	tWriter.mWriter.Flush()
+	return nil
+}
+
+// 关闭文件读取器
+func (tWriter *CsvWriter) Close() []string {
+	// 文件写入缓存推送
+	tWriter.mWriter.Flush()
+
+	// 关闭文件句柄
+	if nil != tWriter.mFileHandle {
+		tWriter.mFileHandle.Close()
+	}
+
+	return tWriter.mFileList
+}
+
+// 快速保存CSV文件
+func WriteAll(tFileName string, tRecords [][]string, opts ...WriterOptions) ([]string, error) {
+	// 创建writer
+	tWriter, err := NewWriter(tFileName, opts...)
+	if err != nil {
+		return nil, err
+	}
+	if tWriter != nil {
+		// 如果有文件头
+		if tWriter.mHeader != nil {
+			err := tWriter.Write(tWriter.mHeader)
+			if err != nil {
+				tWriter.mFileHandle.Close()
+				return nil, err
+			}
+		}
+		// 写入所有数据
+		err := tWriter.WriteAll(tRecords)
+		if err != nil {
+			return nil, err
+		}
+		// 关闭
+		return tWriter.Close(), nil
+	}
+	return nil, errors.New("the writer is nil ")
 }
--- go-code/lib/primCsv/primCsv_test.go
+++ go-code/lib/primCsv/primCsv_test.go
@@ -1,11 +1,11 @@
 package primCsv
 
 import (
+	"strconv"
 	"testing"
 )
 
-// 测试总入口
-func Test(t *testing.T) {
+func test_read(t *testing.T) {
 	tReader, err := Open(`C:\Users\007\Downloads/dddddddddddddddddd.csv`, Options{HaveHeader: true})
 	if nil != err {
 		t.Logf("open err %s\n", err.Error())
@@ -29,4 +29,65 @@ func Test(t *testing.T) {
 		}
 		t.Logf("FieldsPerRecord=%d data=%#v \n", tReader.mReader.FieldsPerRecord, csvdata)
 	}
+}
+
+func test_write(t *testing.T) {
+	var header []string
+	header = append(header, "id")
+	header = append(header, "num")
+	var tRecords [][]string
+	for i := 0; i < 500000; i++ {
+		var t []string
+		t = append(t, strconv.Itoa(i))
+		t = append(t, strconv.Itoa(i*2))
+		tRecords = append(tRecords, t)
+	}
+
+	// 创建writer
+	{
+		_, err := WriteAll("./WriteAll20221027.csv", tRecords, WriterOptions{
+			//Header:   header,
+			MaxLines: 500000,
+		})
+		if err != nil {
+			t.Logf("WriteAll err=%s\n", err.Error())
+		}
+	}
+	// 创建writer
+	tWriter, err := NewWriter("./test20221027.csv", WriterOptions{
+		Header:   header,
+		MaxLines: 500000,
+	})
+
+	if err != nil {
+		t.Logf("NewWriter err=%s\n", err.Error())
+	}
+
+	if tWriter != nil {
+		if tWriter.mFileHandle == nil {
+			t.Logf("tWriter mFileHandle is nil\n")
+		}
+		{
+			err := tWriter.Write(header)
+			if err != nil {
+				t.Logf("tWriter err=%s\n", err.Error())
+			}
+		}
+		{ // 写入所有数据
+			err := tWriter.WriteAll(tRecords)
+			if err != nil {
+				t.Logf("tWriter err=%s\n", err.Error())
+			}
+		}
+
+		// 关闭
+		tWriter.Close()
+	}
+
+}
+
+// 测试总入口
+func Test(t *testing.T) {
+	test_read(t)
+	test_write(t)
 }
