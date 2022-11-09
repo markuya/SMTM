fe3a3af913fb8aecec187dfcef024fde6de61e68
赵睿 <zhaorui826@qq.com>
2022-11-09 20:02:22 +0800

    行情结构定义

--- go-code/app/services/HisQuotes/HisQuotesDef.go
+++ go-code/app/services/HisQuotes/HisQuotesDef.go
@@ -1,7 +1,19 @@
 package HisQuotes
 
+import (
+	"time"
+)
+
 // 行情信息
 type Quote struct {
+	Date     *time.Time // 日期
+	AdjClose float32    // 调整收盘价格
+	Close    float32    // 收盘价格
+	High     float32    // 最高
+	Low      float32    // 最低
+	Open     float32    // 开盘价格
+	Volume   int64      // 成交量
+	Amount   float64    // 成交额
 }
 
 type Stock struct {
